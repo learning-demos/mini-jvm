3cec74247e20edc11a04b2643b012b2435f1d353
DieselNiu <niu853273494@qq.com>
2022-03-12 21:55:19 +0800

    DONE: getstatic

--- src/main/java/com/github/dieselniu/MiniJVM.java
+++ src/main/java/com/github/dieselniu/MiniJVM.java
@@ -4,6 +4,11 @@ import com.github.zxh.classpy.classfile.ClassFile;
 import com.github.zxh.classpy.classfile.ClassFileParser;
 import com.github.zxh.classpy.classfile.MethodInfo;
 import com.github.zxh.classpy.classfile.bytecode.Instruction;
+import com.github.zxh.classpy.classfile.bytecode.InstructionCp2;
+import com.github.zxh.classpy.classfile.constant.ConstantClassInfo;
+import com.github.zxh.classpy.classfile.constant.ConstantFieldrefInfo;
+import com.github.zxh.classpy.classfile.constant.ConstantNameAndTypeInfo;
+import com.github.zxh.classpy.classfile.constant.ConstantPool;
 
 import java.io.File;
 import java.io.IOException;
@@ -25,6 +30,7 @@ public class MiniJVM {
 	 * @param classPath 启动时的classpath，使用{@Link java.io.File.pathSeparator},只支持文件夹
 	 * @param mainClass
 	 */
+
 	public MiniJVM(String classPath, String mainClass) {
 		this.mainClass = mainClass;
 		this.classPathEntries = classPath.split(File.pathSeparator);
@@ -56,6 +62,25 @@ public class MiniJVM {
 			}
 			switch (instruction.getOpcode()) {
 				case getstatic:
+					int fieldIndex = InstructionCp2.class.cast(instruction).getTargetFieldIndex();
+					ConstantPool constantPool = mainClassFile.getConstantPool();
+					ConstantFieldrefInfo fieldrefInfo = constantPool.getFieldrefInfo(fieldIndex);
+					ConstantClassInfo classInfo = fieldrefInfo.getClassInfo(constantPool);
+					ConstantNameAndTypeInfo nameAndTypeInfo = fieldrefInfo.getFieldNameAndTypeInfo(constantPool);
+
+					String className = constantPool.getUtf8String(classInfo.getNameIndex());
+					String fieldName = nameAndTypeInfo.getName(constantPool);
+					String fieldType = nameAndTypeInfo.getType(constantPool);
+
+					if ("/java/lang/System".equals(className) && "out".equals(fieldName)) {
+						Object field = System.out;
+						pcRegister.getTopFrame().pushObjectToOperandStack(field);
+
+					} else {
+						throw new IllegalStateException("Not implemented yet!");
+					}
+
+
 				case invokestatic:
 				case invokevirtual:
 				case _return:
@@ -90,6 +115,11 @@ public class MiniJVM {
 			this.methodStack = methodStack;
 		}
 
+
+		public StackFrame getTopFrame() {
+			return methodStack.peek();
+		}
+
 		public Instruction getNextInstruction() {
 			if (methodStack.isEmpty()) {
 				return null;
@@ -102,7 +132,7 @@ public class MiniJVM {
 
 	static class StackFrame {
 		Object[] localVariables;
-		Stack<Objects> operandStack;
+		Stack<Object> operandStack;
 		MethodInfo methodInfo;
 		int currentInstructionIndex;
 
@@ -113,6 +143,11 @@ public class MiniJVM {
 		public StackFrame(Object[] localVariables, MethodInfo methodInfo) {
 			this.localVariables = localVariables;
 			this.methodInfo = methodInfo;
+		}
+
+
+		public void pushObjectToOperandStack(Object object) {
+			operandStack.push(object);
 		}
 	}
 }
